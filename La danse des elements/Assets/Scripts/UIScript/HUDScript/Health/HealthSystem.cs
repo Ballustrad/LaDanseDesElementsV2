8642e0ea06c578395b302e50c3f259cc7371b01b
Ballustrad <ethan.azoulay@e-artsup.net>
2023-12-12 17:53:23 +0100

    mouillé

--- La danse des elements/Assets/Scripts/UIScript/HUDScript/Health/HealthSystem.cs
+++ La danse des elements/Assets/Scripts/UIScript/HUDScript/Health/HealthSystem.cs
@@ -2,6 +2,7 @@ using KinematicCharacterController.Examples;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UI;
 
 public class HealthSystem : MonoBehaviour
@@ -12,14 +13,30 @@ public class HealthSystem : MonoBehaviour
     public HealthBar healthBar;
     public ExamplePlayer player;
     public int numberofDotFire = 0;
+    public NavMeshAgent navMeshAgent;
+
     private void Start()
     {
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     private void Update()
     {
+        if ( hasEvaporate == true )
+        {
+            evaporateTimer -= Time.deltaTime;
+        }
+        if ( evaporateTimer < 0 )
+        {
+            hasEvaporate = false;
+        }
+        if (isWet == true && numberofDotFire > 0 && hasEvaporate == false)
+        {
+            Evaporate();
+        }
+        print(isWet);
         if (Input.GetKeyDown(KeyCode.KeypadMinus))
         {
             TakeDamage(20);
@@ -31,6 +48,16 @@ public class HealthSystem : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.KeypadPlus))
         {
            HealHealth(20);
+        }
+        while (isWet == true)
+        {
+            navMeshAgent.speed = navMeshAgent.speed - 3f;
+            isWetTimer -= Time.deltaTime;
+            if (isWetTimer < 0) 
+            { 
+                isWet = false;
+                navMeshAgent.speed = 11;
+            }
         }
 
         if (Time.time - lastDotTick >= 1f)
@@ -46,6 +73,14 @@ public class HealthSystem : MonoBehaviour
         //Tue le joueur si sa vie atteint 0
         if (currentHealth == 0) { Death(); }
     }
+    public bool hasEvaporate;
+    public float evaporateTimer;
+    public void Evaporate()
+    {
+        TakeDamage(20);
+        hasEvaporate = true;
+        evaporateTimer = 5f;
+    }
     public float fireDOTdmg = 0;    
     public float lastDotTick = 0;
     public void InflictDotDmg()
@@ -56,18 +91,19 @@ public class HealthSystem : MonoBehaviour
     }
     public bool isWet = false;
     public float isWetTimer = 0;
-    public void addWetEffect()
+   
+    public void WetRefresh()
     {
         if (isWet == false)
         {
             isWet = true;
+            isWetTimer = 4;
         }
-        else
+        if (isWet == true)
         {
-
+            isWetTimer = 4;
         }
     }
-  
     
     
     public void addFireDot()
