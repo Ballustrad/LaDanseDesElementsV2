386f0a6d453202eb9106c33df891a90f05b13a68
Galaan <galaan2004@gmail.com>
2023-12-19 16:55:11 +0100

    Update Menu

--- La danse des elements/Assets/Scripts/UIScript/MenuScript/PauseMenu.cs
+++ La danse des elements/Assets/Scripts/UIScript/MenuScript/PauseMenu.cs
@@ -8,43 +8,26 @@ public class PauseMenu : MonoBehaviour
 {
     public static bool gameIsPaused = false;
     public static bool menuContenuCheck = false;
-    public static bool loadMenuCheck = false;
     public static bool F11 = false;
 
-    [Header("Scene Management")]
-    //public string sceneActuelle;
-    //public string sceneAChargerSiIdentique;
-    //public string sceneAChargerSiDifferent;
-    public string sceneToReload;
-
-
     [Header("MENU")]
+    public GameObject menus;
     public GameObject pauseMenu;
-    public GameObject menuContenu;
     [Space(10)]
     public Button primaryPauseMenuButton;
-    [Space(20)]
-
-    [Header("Load MENU")]
-    public GameObject loadMENU;
-    [Space(10)]
-    public Button primaryLoadMenuButton;
     [Space(20)]
 
     [Header("Options MENU")]
     public GameObject optionsMENU;
     [Space(10)]
-    public GameObject audiosBoard;
-    public GameObject graphicsBoard;
-    public GameObject controlsBoard;
-    [Space(20)]
     public Button primaryOptionsMenuButton;
-    [Space(10)]
-    public Button primaryControlsButton;
-    public Button primaryAudiosButton;
-    public Button primaryGraphicsButton;
 
     //Pause - Resume
+    private void Start()
+    {
+        primaryPauseMenuButton.Select();
+    }
+
     void LateUpdate()                                                                                 // Updates Every Frame After All Other Updates Have Been Completed.
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -61,14 +44,6 @@ public class PauseMenu : MonoBehaviour
                 primaryPauseMenuButton.Select();
             }
         }     
-        if (Input.GetKeyDown(KeyCode.KeypadEnter)) 
-        {
-            if (gameIsPaused == false)
-            {
-                SceneManager.LoadScene(sceneToReload);
-            }
-        }
-
         if (Input.GetKeyDown(KeyCode.F11))
         {
             if (F11 == false)
@@ -85,7 +60,7 @@ public class PauseMenu : MonoBehaviour
     }
     void Paused()
     {
-        pauseMenu.SetActive(true);
+        menus.SetActive(true);
 
         Time.timeScale = 0;
         gameIsPaused = true;
@@ -98,7 +73,7 @@ public class PauseMenu : MonoBehaviour
     }
     public void Resume()
     {
-        pauseMenu.SetActive(false);
+        menus.SetActive(false);
 
         Time.timeScale = 1;
         gameIsPaused = false;
@@ -108,39 +83,19 @@ public class PauseMenu : MonoBehaviour
 
         Debug.Log("Resume");
 
-        CloseLoadMENU();
         CloseOptions();
     }
 
 
     //MENU
-    public void LoadMENU()
-    {
-        loadMENU.SetActive(true);
-        menuContenu.SetActive(false);
-        primaryLoadMenuButton.Select();
-    }
-
-
     public void OptionsMENU()
     {
         optionsMENU.SetActive(true);
         primaryOptionsMenuButton.Select();
 
-        controlsBoard.SetActive(true);
-
-        audiosBoard.SetActive(false);
-        graphicsBoard.SetActive(false);
-
-
-        menuContenu.SetActive(false);
+        pauseMenu.SetActive(false);
     }
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
-
-    //LoadMENU
+    
     public void MainMenuPlay()
     {
         Resume();
@@ -148,55 +103,18 @@ public class PauseMenu : MonoBehaviour
         Cursor.lockState = CursorLockMode.None;
         SceneManager.LoadScene("Menu");
     }
-    public void TrainingRoomPlay()
-    {
-        Resume();
-        SceneManager.LoadScene("TrainingRoom");
-    }
-    public void CloseLoadMENU()
-    {
-        loadMENU.SetActive(false);
 
-
-        menuContenu.SetActive(true);
-        primaryPauseMenuButton.Select();
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
-    //OptionsMENU
-    public void ControlsBoard()
-    {
-        controlsBoard.SetActive(true);
-        primaryControlsButton.Select();
-
-        audiosBoard.SetActive(false);
-        graphicsBoard.SetActive(false);
-    }
-    public void AudiosBoard()
-    {
-        audiosBoard.SetActive(true);
-        primaryAudiosButton.Select();
-
-        graphicsBoard.SetActive(false);
-        controlsBoard.SetActive(false);
-    }
-    public void GraphicsBoard()
-    {
-        graphicsBoard.SetActive(true);
-        primaryGraphicsButton.Select();
-
-        audiosBoard.SetActive(false);
-        controlsBoard.SetActive(false);
-    }
-
+    // Options
     public void CloseOptions()
     {
         optionsMENU.SetActive(false);
 
-        audiosBoard.SetActive(false);
-        graphicsBoard.SetActive(false);
-
-
-        menuContenu.SetActive(true);
+        pauseMenu.SetActive(true);
         primaryPauseMenuButton.Select();
     }
 }
